bed2ef386e87ef4ab41773d94579b36144308453
Agustin Valenzuela <agus.valenzuela@alumnos.duoc.cl>
2020-11-09 18:06:56 -0300

    Agregada la info y foto de agustin

--- docs/JS/fade.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        $("#carouselExampleIndicators").css("opacity", 1 - $(window).scrollTop() / $('#carouselExampleIndicators').height());
-    });
-});
